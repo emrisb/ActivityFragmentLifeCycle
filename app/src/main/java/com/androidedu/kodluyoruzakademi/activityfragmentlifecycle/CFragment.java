a67c94233fdb6539f7cda666465d11bdc91f362a
Emre İşbaralı <emre.isbarali@gmail.com>
2017-07-29 03:07:02 +0300

    Resimler drawable klasörüne eklendi işlem tamam.

---
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/activityfragmentlifecycle/CFragment.java
@@ -0,0 +1,67 @@
+package com.androidedu.kodluyoruzakademi.activityfragmentlifecycle;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CFragment extends Fragment {
+
+    private final String MAIN_TAG = "CFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        printLog(MAIN_TAG, "Before onCreate");
+        super.onCreate(savedInstanceState);
+        printLog(MAIN_TAG, "After onCreate");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        printLog(MAIN_TAG, "Before onCreateView");
+        View view = inflater.inflate(R.layout.fragment_c, container, false);
+        printLog(MAIN_TAG, "After onCreateView");
+        return view;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        printLog(MAIN_TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        printLog(MAIN_TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        printLog(MAIN_TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        printLog(MAIN_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        printLog(MAIN_TAG, "onDestroy");
+    }
+
+    public String printLog(String TAG, String methodName) {
+        Log.e(TAG, methodName);
+        return methodName;
+    }
+}
--- app/src/main/java/com/androidedu/kodluyoruzakademi/activityfragmentlifecycle/MixedLifeCycleActivity.java
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/activityfragmentlifecycle/MixedLifeCycleActivity.java
@@ -1,13 +1,92 @@
 package com.androidedu.kodluyoruzakademi.activityfragmentlifecycle;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MixedLifeCycleActivity extends AppCompatActivity {
 
+    private final String MAIN_TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        printLog(MAIN_TAG, "Before onCreate");
         setContentView(R.layout.activity_mixed_life_cycle);
+        printLog(MAIN_TAG, "After onCreate");
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        CFragment cFragment = new CFragment();
+        fragmentTransaction.add(R.id.dynamicFragment, cFragment);
+        fragmentTransaction.commit();
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        printLog(MAIN_TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        printLog(MAIN_TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        printLog(MAIN_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        printLog(MAIN_TAG, "onPause");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        printLog(MAIN_TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        printLog(MAIN_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        printLog(MAIN_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        printLog(MAIN_TAG, "onRestart");
+    }
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+        printLog(MAIN_TAG, "onAttachFragment");
+    }
+
+    public String printLog(String TAG, String methodName) {
+        Log.e(TAG, methodName);
+        return methodName;
+    }
+
+
+
 }
